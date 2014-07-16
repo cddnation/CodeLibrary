824b4b2ab4259eea19b2e99947985a97bb322c10
FabienO <fabienoram@gmail.com>
2014-07-16 12:49:06 +0100

    Hooker was a bit dirty so I cleaned it up.

--- Git/Hooker/puller.php
+++ Git/Hooker/puller.php
@@ -1,40 +1,43 @@
 <?php
+$branch = 'master';
+
+$start = time();
+
 while(1) {
-    if(file_exists('/tmp/fab.txt')) {
-        echo "File found\n";
-        // Read file
-        $json = file_get_contents('/tmp/fab.txt');
-        $pushData = json_decode($json);
-
-        $pull = false;
-        $commitSHA = '';
-        $branch = 'master';
-
-        // Check if there's a pull request
-        foreach($pushData->commits as $commit) {
-            if(strpos($commit->message, '-pull-') !== false) {
-                $pull = true;
-                $commitSHA = $commit->id;
-            }
-        }
-
-        // IF so, pull the most recent
-        if($pull && $commitSHA != '') {
-//            $cmd = "git pull origin $commitSHA:$branch";
-            $cmd = "git pull origin $branch";
-            echo $cmd;
-            $out = shell_exec($cmd);
-
-            echo $out;
-            echo 'Pulling:' . $out;
-        } else {
-            echo "No pull request found\n";
-        }
-
-
-
-        unlink('/tmp/fab.txt');
+    // Kill script every hour to keep it dougie-fresh..
+    if((time() - $start) > 3600) {
+        exit;
     }
 
+    $pull = false;
+
+    if(!file_exists('/tmp/fab.txt')) {
+        // File not found skip
+        sleep(1);
+        continue;
+    }
+
+    echo "File found\n";
+
+    $json = file_get_contents('/tmp/fab.txt');
+    $pushData = json_decode($json);
+
+    // Check if there's a pull request
+    foreach($pushData->commits as $commit) {
+        if(strpos($commit->message, '-pull-') !== false) {
+            $pull = true;
+        }
+    }
+
+    // Pull the most recent
+    if($pull) {
+        $out = shell_exec("git pull origin $branch");
+        echo 'Pulling: ' . $out;
+    } else {
+        echo "No pull request found\n";
+    }
+
+    // Cleanup
+    unlink('/tmp/fab.txt');
     sleep(1);
 }
